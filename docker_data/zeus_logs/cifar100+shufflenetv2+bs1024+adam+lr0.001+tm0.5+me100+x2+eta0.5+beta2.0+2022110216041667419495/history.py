b794b0decb063157f950d00599b28e96001ede71
Helen Cho <helencho2014@gmail.com>
2022-11-02 21:13:19 +0000

    Run shufflenetv2 with batch size 512 and 1024

---
+++ docker_data/zeus_logs/cifar100+shufflenetv2+bs1024+adam+lr0.001+tm0.5+me100+x2+eta0.5+beta2.0+2022110216041667419495/history.py
@@ -0,0 +1,2 @@
+[HistoryEntry(bs=1024, pl=None, energy=296319.64810927096, reached=True, time=2039.8214584470002),
+ HistoryEntry(bs=512, pl=None, energy=201651.69326980342, reached=True, time=1382.1591927559984)]
