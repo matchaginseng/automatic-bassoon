497c75d2da74638dc9a8bd390e97776998b7c7f1
Helen Cho <hcho@college.harvard.edu>
2022-11-06 23:43:30 -0500

    Try to add target metric to power limit stuff

--- examples/cifar100/run_shufflenetv2_pl.py
+++ examples/cifar100/run_shufflenetv2_pl.py
@@ -2,13 +2,17 @@
 """Script for running Zeus on the CIFAR100 dataset to train Shufflenet V2."""
 
 import argparse
+import json
+import pprint
 import sys
 from pathlib import Path
+import os
+import subprocess
+from copy import deepcopy
+from time import sleep
 
 from zeus.job import Job
-from zeus.policy import PruningGTSBatchSizeOptimizer
-from zeus.run import ZeusMaster
-from zeus.util import FileAndConsole
+from zeus.analyze import HistoryEntry
 
 
 def parse_args() -> argparse.Namespace:
@@ -25,6 +29,7 @@ def parse_args() -> argparse.Namespace:
     # The first recurrence uses these parameters, and it must reach the target metric.
     parser.add_argument("--b_0", type=int, default=1024, help="Default batch size")
     parser.add_argument("--lr_0", type=float, default=0.001, help="Default learning rate")
+    parser.add_argument("--pl_0", type=int, default=175000, help="Default power limit (mW)")
 
     # The range of batch sizes to consider. The example script generates a list of power-of-two
     # batch sizes, but batch sizes need not be power-of-two for Zeus.
@@ -33,6 +38,14 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--b_max", type=int, default=4096, help="Largest batch size to consider"
+    )
+
+    # The range of power limits to consider. Generates a list of power limits of every 25 W
+    parser.add_argument(
+        "--pl_min", type=int, default=100000, help="Smallest power limit to consider, in mW"
+    )
+    parser.add_argument(
+        "--pl_max", type=int, default=175000, help="Largest power limit to consider, in mW"
     )
 
     # The total number of recurrences.
@@ -64,35 +77,12 @@ def parse_args() -> argparse.Namespace:
 
 
 def main(args: argparse.Namespace) -> None:
-    """Run Zeus on CIFAR100."""
-    # Zeus's batch size optimizer.
-    # First prunes unpromising batch sizes, and then runs Gaussian Thompson Sampling MAB.
-    bso = PruningGTSBatchSizeOptimizer(seed=args.seed, verbose=True)
-
-    # The top-level class for running Zeus.
-    # - The batch size optimizer is desinged as a pluggable policy.
-    # - Paths (log_base and monitor_path) assume our Docker image's directory structure.
-    # - For CIFAR100, one epoch of training may not take even ten seconds (especially for
-    #   small models like squeezenet). ZeusDataLoader automatically handles profiling power
-    #   limits over multiple training epochs such that the profiling window of each power
-    #   limit fully fits in one epoch. However, the epoch duration may be so short that
-    #   profiling even one power limit may not fully fit in one epoch. In such cases,
-    #   ZeusDataLoader raises a RuntimeError, and the profiling window should be narrowed
-    #   by giving smaller values to profile_warmup_iters and profile_measure_iters in the
-    #   constructor of ZeusMaster.
-    master = ZeusMaster(
-        batch_size_optimizer=bso,
-        log_base="/workspace/zeus_logs",
-        seed=args.seed,
-        monitor_path="/workspace/zeus/zeus_monitor/zeus_monitor",
-        observer_mode=False,
-        profile_warmup_iters=10,
-        profile_measure_iters=40,
-    )
+    """Run Profiler on CIFAR100."""
 
     # Definition of the CIFAR100 job.
     # The `Job` class encloses all information needed to run training. The `command` parameter is
     # a command template. Curly-braced parameters are recognized by Zeus and automatically filled.
+
     job = Job(
         dataset="cifar100",
         network="shufflenetv2",
@@ -106,19 +96,113 @@ def main(args: argparse.Namespace) -> None:
         command=[
             "python",
             "train.py",
-            "--zeus",
+            "--profile",
             "--arch", "shufflenetv2",
-            "--batch_size", "{batch_size}",
+            "--batch_size", "{args.b_0}",
             "--epochs", "{epochs}",
-            "--seed", "{seed}"
+            "--seed", "{seed}",
+            "--power_limit", "{args.pl_0}"
         ],
         # fmt: on
     )
+    
+    # Run the job! Code hacked together from master.py, run_job()
+
+    # Generate job command
+    command = job.gen_command(args.b_0, args.lr_0, args.seed, 0)
+
+    # Set environment variables
+    job_id = f"cifar100+shufflenetv2+bs{args.b_0}+pl{args.pl_0}"
+    logdir = "/workspace/examples/cifar100"
+    zeus_env = dict(
+        ZEUS_LOG_PREFIX=str(job_id),
+        ZEUS_TARGET_METRIC=str(job.target_metric),
+        ZEUS_MONITOR_PATH="/workspace/zeus/zeus_monitor/zeus_monitor",
+        ZEUS_MONITOR_SLEEP_MS="100"
+    )
+    env = deepcopy(os.environ)
+    env.update(zeus_env)
+
+    # Training script output captured by the master.
+    job_output = f"{job_id}.train.log"
+
+    # Training stats (energy, time, reached, end_epoch) written by ZeusDataLoader.
+    # This file being found means that the training job is done.
+    train_json = Path(f"{logdir}/{job_id}.train.json")
+
+    # Job history list to return.
+    history: list[HistoryEntry] = []
+
+    # Save job history to this file, continuously.
+    history_file = f"{logdir}/history+{job_id}.py"
+
+    # Reporting
+    print(f"[run job] Launching job with BS {args.b_0}:")
+    print(f"[run job] {zeus_env=}")
+    if job.workdir is not None:
+        print(f"[run job] cwd={job.workdir}")
+    print(f"[run job] {command=}")
+    # print(f"[run job] {cost_ub=}")
+    print(f"[run job] Job output logged to '{job_output}'")
+
+    # Run the job.
+    with open(job_output, "w") as f:
+        # Launch subprocess.
+        # stderr is redirected to stdout, and stdout to the job_output file.
+        proc = subprocess.Popen(
+            command,
+            cwd=job.workdir,
+            stderr=subprocess.STDOUT,
+            stdout=f,
+        )
+
+        # Check if training is done.
+        with open(job_output, "r") as jobf:
+            while proc.poll() is None:
+                print(jobf.read(), end="")
+                sleep(1.0)
+
+            # Print out the rest of the script output.
+            f.flush()
+            print(jobf.read())
+
+            # Report exitcode.
+            exitcode = proc.poll()
+            print(f"[run job] Job terminated with exit code {exitcode}.")
+
+        # `train_json` must exist at this point.
+        if not train_json.exists():
+            raise RuntimeError(f"{train_json} does not exist.")
+
+    # Read `train_json` for the training stats.
+    with open(train_json, "r") as f:
+        stats = json.load(f)
+        print(f"[run job] {stats=}")
+    train_json.close()
+
+    # Casting
+    if not isinstance(stats["reached"], bool):
+        stats["reached"] = stats["reached"].lower() == "true"
+
+    # Record history for visualization.
+    history.append(HistoryEntry(args.b_0, args.pl_0, float(stats["energy"]), stats["reached"], float(stats["time"])))
+    with open(history_file, "w") as f:
+        # Intended use:
+        #
+        # ```python
+        # from zeus.analyze import HistoryEntry
+        # history = eval(open(history_file).read())
+        # ```
+        f.write(pprint.pformat(history) + "\n")
+    history_file.close()
+
+    # return float(stats["energy"]), float(stats["time"]), stats["reached"]
+
 
     # Generate a list of batch sizes with only power-of-two values.
-    batch_sizes = [args.b_min]
-    while (bs := batch_sizes[-1] * 2) <= args.b_max:
-        batch_sizes.append(bs)
+    # batch_sizes = [args.b_min]
+    # while (bs := batch_sizes[-1] * 2) <= args.b_max:
+    #     batch_sizes.append(bs)
 
     # Create a designated log directory inside `args.log_base` just for this run of Zeus.
     # Six types of outputs are generated.
@@ -139,26 +223,28 @@ def main(args: argparse.Namespace) -> None:
     # 6. Job history (`history.py`):
     #      A python file holding a list of `HistoryEntry` objects. Intended use is:
     #      `history = eval(open("history.py").read())` after importing `HistoryEntry`.
-    master_logdir = master.build_logdir(
-        job=job,
-        num_recurrence=args.num_recurrence,
-        eta_knob=args.eta_knob,
-        beta_knob=args.beta_knob,
-        exist_ok=False,  # Should err if this directory exists.
-    )
+    # master_logdir = master.build_logdir(
+    #     job=job,
+    #     num_recurrence=args.num_recurrence,
+    #     eta_knob=args.eta_knob,
+    #     beta_knob=args.beta_knob,
+    #     exist_ok=False,  # Should err if this directory exists.
+    # )
 
     # Overwrite the stdout file descriptor with an instance of `FileAndConsole`, so that
     # all calls to `print` will write to both the console and the master log file.
-    sys.stdout = FileAndConsole(Path(master_logdir) / "master.log")
+    # sys.stdout = FileAndConsole(Path(master_logdir) / "master.log")
+
+
 
     # Run Zeus!
-    master.run(
-        job=job,
-        num_recurrence=args.num_recurrence,
-        batch_sizes=batch_sizes,
-        beta_knob=args.beta_knob,
-        eta_knob=args.eta_knob,
-    )
+    # master.run(
+    #     job=job,
+    #     num_recurrence=args.num_recurrence,
+    #     batch_sizes=batch_sizes,
+    #     beta_knob=args.beta_knob,
+    #     eta_knob=args.eta_knob,
+    # )
 
 
 if __name__ == "__main__":
