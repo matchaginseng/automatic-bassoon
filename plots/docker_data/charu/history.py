b00833402e8d1f2319804450259c821811cdffc3
Ubuntu <ubuntu@ip-172-31-63-72.ec2.internal>
2022-11-24 00:52:16 +0000

    plots

---
+++ plots/docker_data/charu/history.py
@@ -0,0 +1,4 @@
+[HistoryEntry(bs=8, pl=None, energy=1607.2607641892432, reached=True, time=17.765798296999947),
+ HistoryEntry(bs=8, pl=None, energy=1693.3648830980062, reached=True, time=17.390340226999797),
+ HistoryEntry(bs=8, pl=None, energy=1654.4937768933773, reached=True, time=17.05265957599977),
+ HistoryEntry(bs=8, pl=None, energy=1657.3292302720547, reached=True, time=16.931840635999833)]
