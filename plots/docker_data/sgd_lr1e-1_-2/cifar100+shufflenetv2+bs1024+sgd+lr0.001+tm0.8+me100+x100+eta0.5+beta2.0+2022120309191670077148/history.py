b9ade630894d993abf6a49a4df0a736fdf19edad
Helen Cho <helencho2014@gmail.com>
2022-12-03 19:43:43 +0000

    sgd lr=0.01 and 0.1 experiment data

---
+++ plots/docker_data/sgd_lr1e-1_-2/cifar100+shufflenetv2+bs1024+sgd+lr0.001+tm0.8+me100+x100+eta0.5+beta2.0+2022120309191670077148/history.py
@@ -0,0 +1,2 @@
+[HistoryEntry(bs=128, pl=175, lr=0.01, energy=1142894.1535509098, reached=False, time=8668.11462892301),
+ HistoryEntry(bs=128, pl=150, lr=0.1, energy=1143287.429464015, reached=False, time=8670.460362390906)]
