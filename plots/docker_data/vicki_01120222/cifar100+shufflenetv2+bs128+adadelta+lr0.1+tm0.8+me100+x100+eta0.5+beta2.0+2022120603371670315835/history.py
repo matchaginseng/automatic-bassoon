4df6a85ef394689da98ee60ee04d2582257568f2
Ubuntu <ubuntu@ip-172-31-63-179.ec2.internal>
2022-12-06 13:41:34 +0000

    new adadelta data

---
+++ plots/docker_data/vicki_01120222/cifar100+shufflenetv2+bs128+adadelta+lr0.1+tm0.8+me100+x100+eta0.5+beta2.0+2022120603371670315835/history.py
@@ -0,0 +1,2 @@
+[HistoryEntry(bs=128, pl=None, lr=0.01, energy=1095198.162072911, reached=False, time=9021.515720660003),
+ HistoryEntry(bs=128, pl=None, lr=0.01, energy=1094989.2070152608, reached=False, time=9021.051456862002)]
