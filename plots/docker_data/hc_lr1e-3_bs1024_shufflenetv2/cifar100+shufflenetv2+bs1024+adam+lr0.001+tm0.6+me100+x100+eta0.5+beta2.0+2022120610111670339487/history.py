3b340a4dd01ffec82ba30c77e46bce7610e77454
Helen Cho <helencho2014@gmail.com>
2022-12-06 23:22:47 +0000

    data from learning rate exp for adam, bs=1024 for shufflenetv2

---
+++ plots/docker_data/hc_lr1e-3_bs1024_shufflenetv2/cifar100+shufflenetv2+bs1024+adam+lr0.001+tm0.6+me100+x100+eta0.5+beta2.0+2022120610111670339487/history.py
@@ -0,0 +1,1 @@
+[HistoryEntry(bs=1024, pl=None, lr=0.001, energy=672462.2731224052, reached=False, time=4806.314337210996)]
---
+++ plots/docker_data/hc_remaining_lr_adam_shufflenetv2_bs1024/cifar100+shufflenetv2+bs1024+adam+lr0.001+tm0.6+me100+x100+eta0.5+beta2.0+2022120611401670344805/history.py
@@ -0,0 +1,4 @@
+[HistoryEntry(bs=1024, pl=175.0, lr=0.005, energy=240343.41170650077, reached=True, time=1745.0100623509989),
+ HistoryEntry(bs=1024, pl=175.0, lr=0.01, energy=512864.726165234, reached=True, time=3675.9740900210018),
+ HistoryEntry(bs=1024, pl=150.0, lr=0.05, energy=673308.1082976655, reached=False, time=4862.071877154984),
+ HistoryEntry(bs=1024, pl=150.0, lr=0.5, energy=630375.3421898179, reached=False, time=4775.313635210998)]
