f8fac1d94dd8afe88c3a0554f66ff7f5fb9b65d6
Helen Cho <helencho2014@gmail.com>
2022-12-09 01:16:35 +0000

    Add lr=0.006, 0.007, 0.008, 0.009 with bs1024 and adam shufflenetv2 data

---
+++ plots/docker_data/hc_finegrained_lr_bs1024_shufflenetv2/cifar100+shufflenetv2+bs1024+adam+lr0.001+tm0.6+me100+x100+eta0.5+beta2.0+2022120817401670539224/history.py
@@ -0,0 +1,4 @@
+[HistoryEntry(bs=1024, pl=175.0, lr=0.006, energy=245090.3820494143, reached=True, time=1682.810654917026),
+ HistoryEntry(bs=1024, pl=175.0, lr=0.007, energy=244904.68639563024, reached=True, time=1681.7110675940203),
+ HistoryEntry(bs=1024, pl=175.0, lr=0.008, energy=361245.8512208044, reached=True, time=2453.759384225974),
+ HistoryEntry(bs=1024, pl=175.0, lr=0.009, energy=469427.69630999834, reached=True, time=3171.8975488980213)]
