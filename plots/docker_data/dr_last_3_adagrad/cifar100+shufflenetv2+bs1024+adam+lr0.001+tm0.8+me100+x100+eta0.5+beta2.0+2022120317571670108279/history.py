c525cc4420e779d18b939fa2ce4c891405fac6b1
Helen Cho <helencho2014@gmail.com>
2022-12-09 05:13:19 +0000

    Adding this dropout data to make the rebase work

---
+++ plots/docker_data/dr_last_3_adagrad/cifar100+shufflenetv2+bs1024+adam+lr0.001+tm0.8+me100+x100+eta0.5+beta2.0+2022120317571670108279/history.py
@@ -0,0 +1,3 @@
+[HistoryEntry(bs=128, pl=175.0, dr=0.5, energy=1162768.9970184802, reached=False, time=8998.66579362491),
+ HistoryEntry(bs=128, pl=150.0, dr=0.7, energy=1161519.9730257555, reached=False, time=9006.13064937394),
+ HistoryEntry(bs=128, pl=150.0, dr=0.9, energy=1161165.554645881, reached=False, time=9010.589056130993)]
