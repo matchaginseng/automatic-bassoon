7fa2fcd826ab1d907d953d04699e08ecffe05603
Helen Cho <hcho@college.harvard.edu>
2022-12-09 23:10:00 -0500

    Make more edits to energy vs time plots

--- plots/shufflenetv2_bs1024_adam_sweep/history_adadelta.py
+++ plots/shufflenetv2_bs1024_adam_sweep/history_adadelta.py
@@ -1,1 +1,2 @@
-[HistoryEntry(bs=1024, pl=175.0, lr=0.01, energy=737225.8546364072, reached=False, time=4940.047461004)]
+[HistoryEntry(bs=1024, pl=175.0, lr=0.01, energy=737225.8546364072, reached=False, time=4940.047461004),
+ HistoryEntry(bs=128, pl=None, lr=None, energy=219024.37, reached=True, time=1806.52)]
